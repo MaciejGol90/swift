ecc9433a721200563d07145141af4de0cc319a1c
Maciej <maciek.golianek@gmail.com>
2019-04-23 17:51:59 +0200

    ex on brown medal

--- MyPlayground.playground/Pages/Conditionals.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Conditionals.xcplaygroundpage/Contents.swift
@@ -1,21 +1,25 @@
 //: [Previous](@previous)
 
 import Foundation
-var population: Int = 5422
+var population: Int = 4422
 var message: String
-var hasPostOffice: Bool
+var hasPostOffice: Bool = true
+
+//message = population < 10000 ? "Populacja \(population) wskazuje na małe miasto." : "Populacja wskazuje na duże miasto"
 
 if population < 10000 {
     message = "Populacja \(population) wzkazuje na male miasto"
-    hasPostOffice = true
-} else {
+} else if population >= 10000 && population < 50000{
+        message = "Populacja wskauje na średniej wielkości miasto"
+    }else if population >= 100000 {
+    message = "Populacja /(population) wskazuje na ogromne miasto"
+} else{
     message = "Populacja \(population) wskazuje na duże miasto"
-    hasPostOffice = false
 }
+
 
 print(message)
 
 if !hasPostOffice {print("Gdzie można kupić znaczki?")}
 
-print("Aloszka Mordeczko")
 //: [Next](@next)
