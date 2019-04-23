f0860fa7fda6231a078ae060f1baad51c1a731c2
Maciej <maciek.golianek@gmail.com>
2019-04-23 14:47:09 +0200

    ex on brown medal

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,4 +2,8 @@ import Cocoa
 let numberOfStopLights: Int = 4
 var population : Int
 population = 5422
-
+let townName: String = "Nigdzie"
+var unemployment: Int
+unemployment = 2
+let townDescription = "Populacja miasta \(townName) wynosi \(population). W mieście znajduą się \(numberOfStopLights) skrzyżowania z sygnalizacją świetlną. W mieście \(townName) bezrobocie wynosi \(unemployment)"
+print(townDescription)
