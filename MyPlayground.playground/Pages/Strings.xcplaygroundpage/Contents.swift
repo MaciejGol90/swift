8e6c06549608992093a0ffc91235c42fee997d91
Maciej <maciek.golianek@gmail.com>
2019-04-25 21:15:41 +0200

    strings brown medal in progress

--- MyPlayground.playground/Pages/Strings.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Strings.xcplaygroundpage/Contents.swift
@@ -1,13 +1,57 @@
 import Cocoa
 
-let playground = "Witaj w playground!"
-var mutablePlayground = "Witaj w modyfikowalnym playground"
-mutablePlayground += " !"
-let oneCoolDude = "\u{1F60E}"
-let aAcute = "\u{0061}\u{0301}"
-for scalar in playground.unicodeScalars {
-    print("\(scalar.value)")
-}
-let aAcutePrecomposed = "\u{00E1}"
-let b = (aAcute == aAcutePrecomposed)
-print
+//let playground = "Witaj w playground!"
+//var mutablePlayground = "Witaj w modyfikowalnym playground"
+//mutablePlayground += " !"
+//
+//for character in mutablePlayground {
+//    print(character)
+//}
+//let oneCoolDude = "\u{1F60E}"
+//let aAcute = "\u{0065}"
+//for scalar in playground.unicodeScalars {
+//    print("\(scalar.value)")
+//}
+//let aAcutePrecomposed = "\u{00E1}"
+//let b = (aAcute == aAcutePrecomposed)
+//
+//print ("aAcute: \(aAcute.count) aAcuteDecomposed: \(aAcutePrecomposed.count)")
+//
+//let fromStart = playground.startIndex
+//let toPosition = 4
+//let end = playground.index(fromStart, offsetBy: toPosition)
+//let fifthCharacter = playground[end]
+//let range = fromStart...end
+//let firstFive = playground[range]
+
+let a = "\u{0061}"
+
+let b = "\u{0062}"
+let c = "\u{0063}"
+let d = "\u{0064}"
+let e = "\u{0065}"
+let f = "\u{0066}"
+let g = "\u{0067}"
+let h = "\u{0068}"
+let i = "\u{0069}"
+let j = "j"
+let k = "\u{0071}"
+let l = "\u{0072}"
+let m = "\u{0073}"
+let n = "\u{0074}"
+let o = "\u{0075}"
+let p = "\u{0076}"
+let r = "\u{0077}"
+let s = "\u{0078}"
+let t = "\u{0079}"
+let u = "\u{0080}"
+let w = "\u{0081}"
+////let convertedW = W.unicodeScalars
+//let stringsBrownMedal = ""
+//print(stringsBrownMedal)
+print(j.unicodeScalars)
+print(\u{0081} + \u{0069} + \u{0079} + \u{0061})
+
+
+
+
