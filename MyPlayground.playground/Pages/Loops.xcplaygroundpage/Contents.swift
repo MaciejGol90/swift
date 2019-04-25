cfe819f34dfc63a87e2ef93a4c8b95961d315835
Maciej <maciek.golianek@gmail.com>
2019-04-25 09:56:11 +0200

    ex on brown medal-loops

--- MyPlayground.playground/Pages/Loops.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Loops.xcplaygroundpage/Contents.swift
@@ -1,42 +1,59 @@
 import Cocoa
 
-// for in
-var myFirstInt: Int = 0
-//for _ in 1...5{
-//    myFirstInt += 1
-//    print(" wartość równa się \(myFirstInt)")
-//}
-//// for case
-//for case let i in 1...100 where i % 3 == 0{
-//    print(i)
-//}
-//while
-//var i = 1
-//while i < 6 {
-//    myFirstInt = 1
-//    print(myFirstInt)
-//    i += 1
-//}
+//// for in
+//var myFirstInt: Int = 0
+////for _ in 1...5{
+////    myFirstInt += 1
+////    print(" wartość równa się \(myFirstInt)")
+////}
+////// for case
+////for case let i in 1...100 where i % 3 == 0{
+////    print(i)
+////}
+////while
+////var i = 1
+////while i < 6 {
+////    myFirstInt = 1
+////    print(myFirstInt)
+////    i += 1
+////}
+////
+////
+//var shields = 5
+//var basterOverHeating = false
+//var blasterFireCount = 0
+//var spaceDemonsDystroed = 0
 //
-//
-var shields = 5
-var basterOverHeating = false
-var blasterFireCount = 0
-while shields < 0{
-    if basterOverHeating {
-        print("Broń jest zagorąca zaczekaj aż się ochłodzi")
-        sleep(5)
-        print("Broń już może śmigać")
-        sleep(1)
-        basterOverHeating = false
-        blasterFireCount = 0
-    }
-    if blasterFireCount > 100{
-    basterOverHeating = true
-    continue
-    }
-    print("Strzelamy")
-    blasterFireCount += 1
+//while shields > 0 {
+//if spaceDemonsDystroed == 50 {
+//    print("Wygrałeś")
+//    break
+//}
+//    if basterOverHeating {
+//        print("Broń jest zagorąca zaczekaj aż się ochłodzi")
+//        sleep(5)
+//        print("Broń już może śmigać")
+//        sleep(1)
+//        basterOverHeating = false
+//        blasterFireCount = 0
+//    }
+//    if blasterFireCount > 10{
+//    basterOverHeating = true
+//    continue
+//    }
+//    print("Strzelamy")
+//    blasterFireCount += 1
+//    spaceDemonsDystroed += 1
+//}
+var liczba: Int = 0
+var i = 1
+while i < 5{
+    i += 1
+print("wykonała się \(i) razy")
+for _ in 0...100{
+    liczba += 2
+    print(liczba)
+    
 }
-
+}
 
