9b4b9358d76d164d2cf4488184fa252db9bfdeef
Maciej <maciek.golianek@gmail.com>
2019-04-30 23:09:20 +0200

    optional

--- MyPlayground.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -1,9 +1,7 @@
 import Cocoa
 var errorCodeString: String?
 errorCodeString = "404"
-if let theError = errorCodeString{
-    if let errorCodeInteger = Int(theError){
-        print("\(theError):\(errorCodeInteger)")
-    }
-
-}
+//if let theError = errorCodeString, let errorCodeInteger = Int(theError), errorCodeInteger == 404 {
+//        print("\(theError):\(errorCodeInteger)")
+//    }
+print (errorCodeString)
