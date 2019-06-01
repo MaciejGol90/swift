d2f3363294f8f975d4c798cd8f00d89ab3caddb9
Maciej <maciek.golianek@gmail.com>
2019-06-01 09:36:33 +0200

    Optional in progress

--- MyPlayground.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -1,7 +1,10 @@
 import Cocoa
 var errorCodeString: String?
 errorCodeString = "404"
-//if let theError = errorCodeString, let errorCodeInteger = Int(theError), errorCodeInteger == 404 {
+if let theError = errorCodeString, let errorCodeInteger = Int(theError), errorCodeInteger == 404 {
 //        print("\(theError):\(errorCodeInteger)")
-//    }
-print (errorCodeString)
+    errorDescryption = ("\(errorCodeInteger + 200): nie znaleziono żadnego zasobu.")
+    }
+var upCaseErrorDescription = errorDescryption?.uppercased()
+errorDescryption
+print (errorCodeString as Any)
