6f931f1e92b1eaf4f36b3bdee94603d9c24e4c64
Maciej <maciek.golianek@gmail.com>
2019-06-03 13:03:28 +0200

    Optional done

--- MyPlayground.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Optionals.xcplaygroundpage/Contents.swift
@@ -1,10 +1,21 @@
 import Cocoa
 var errorCodeString: String?
 errorCodeString = "404"
+var errorDescryption: String?
 if let theError = errorCodeString, let errorCodeInteger = Int(theError), errorCodeInteger == 404 {
 //        print("\(theError):\(errorCodeInteger)")
     errorDescryption = ("\(errorCodeInteger + 200): nie znaleziono żadnego zasobu.")
     }
 var upCaseErrorDescription = errorDescryption?.uppercased()
 errorDescryption
-print (errorCodeString as Any)
+upCaseErrorDescription?.append("Prosze sprobować ponownie.")
+upCaseErrorDescription
+//let descryption: String
+//if let errorDescryption = errorDescryption{
+//    descryption = errorDescryption
+//}else{
+//    descryption = "Brak błędu"
+//}
+errorDescryption = nil
+let descryption = errorDescryption ?? "Brak błędu"
+
