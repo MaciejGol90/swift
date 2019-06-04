4bb9f83676d4a9b284261712d25ba84b9556b903
Maciej <maciek.golianek@gmail.com>
2019-06-04 18:11:23 +0200

    add and delete value in dictonary

--- MyPlayground.playground/Pages/Dictionary.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Dictionary.xcplaygroundpage/Contents.swift
@@ -3,5 +3,10 @@ import Cocoa
 var movieRatings = ["Hotel Transylwania" : 8, "Zwierzogród" : 8, "Avengers End Game": 11]
 print("Oceniłem \(movieRatings.count) filmy")
 let hotelTransylwaniaRating = movieRatings["Hotel Transylwania"]
-movieRatings ["Zwierzogróg"] = 9
+movieRatings ["Zwierzogród"] = 9
+movieRatings
+movieRatings ["Kapitan Marvel"] = 9
+movieRatings
+//movieRatings.removeValue(forKey: "Zwierzogród")
+movieRatings ["Zwierzogród"] = nil
 movieRatings
